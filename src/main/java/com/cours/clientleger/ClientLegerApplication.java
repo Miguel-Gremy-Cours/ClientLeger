7346d7ecc79f68724e9f7fec2b4119b1572d2143
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-09-24 14:28:00 +0200

    Initial commit

---
+++ src/main/java/com/cours/clientleger/ClientLegerApplication.java
@@ -0,0 +1,13 @@
+package com.cours.clientleger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientLegerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientLegerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cours/clientleger/ClientLegerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cours.clientleger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientLegerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
