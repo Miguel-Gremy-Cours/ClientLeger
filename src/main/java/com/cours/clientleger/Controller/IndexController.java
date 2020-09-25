9c5650379c9112c38326cfbb1820a7c10e85082a
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-09-25 15:50:08 +0200

    --Add full htmp template --Add config for DataBase

---
+++ src/main/java/com/cours/clientleger/Controller/IndexController.java
@@ -0,0 +1,28 @@
+package com.cours.clientleger.Controller;
+
+import com.cours.clientleger.Model.Page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class IndexController {
+    @GetMapping()
+    public ModelAndView Index(HttpSession httpSession){
+        Page page = new Page();
+        page.setTitle("Home");
+        page.setPagePath("page/home/Home");
+
+        return refresh(page);
+    }
+
+    private ModelAndView refresh(Page page){
+        ModelAndView model = new ModelAndView("fragment/mainBody");
+        model.addObject(page);
+
+        return model;
+    }
+}
---
+++ src/main/java/com/cours/clientleger/Model/Page.java
@@ -0,0 +1,23 @@
+package com.cours.clientleger.Model;
+
+public class Page {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    private String pagePath;
+
+    public String getPagePath() {
+        return pagePath;
+    }
+
+    public void setPagePath(String pagePath) {
+        this.pagePath = pagePath;
+    }
+}
