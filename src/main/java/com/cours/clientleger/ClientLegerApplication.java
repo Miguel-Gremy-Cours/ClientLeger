6793e30c939afdc7ac7cf8ee586d69508004258b
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-09-28 16:38:16 +0200

    --Add a css file forglobal look --Add bootstrap dependancy --Change home page and header part

--- src/main/java/com/cours/clientleger/ClientLegerApplication.java
+++ src/main/java/com/cours/clientleger/ClientLegerApplication.java
@@ -1,13 +1,18 @@
 package com.cours.clientleger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ClientLegerApplication {
+    @Autowired
+    private static Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(ClientLegerApplication.class, args);
     }
+
 
 }
