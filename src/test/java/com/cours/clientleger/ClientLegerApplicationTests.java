fefa558544a49afded2e70cdb2c99c78606ee73f
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-10-01 11:16:05 +0200

    --Add test for date and find solutionÃ

--- src/test/java/com/cours/clientleger/ClientLegerApplicationTests.java
+++ src/test/java/com/cours/clientleger/ClientLegerApplicationTests.java
@@ -3,11 +3,20 @@ package com.cours.clientleger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
 @SpringBootTest
 class ClientLegerApplicationTests {
 
     @Test
     void contextLoads() {
+        //TODO Put that in main code and in classes
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        LocalDate myDateObj = LocalDate.parse("01/10/2020".replace("/", "-"), myFormatObj);
+        System.out.println("After formatting: " + myDateObj);
     }
 
 }
