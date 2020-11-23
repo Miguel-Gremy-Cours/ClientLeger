7e5d6ff744f41e1b70861f4daf92d945858b87f4
Miguel-Gremy <miguel.gremy72@gmail.com>
2020-11-23 14:11:32 +0100

    Comment Utils file

--- src/main/java/com/cours/clientleger/Utils/DateUtils.java
+++ src/main/java/com/cours/clientleger/Utils/DateUtils.java
@@ -7,10 +7,18 @@ import java.time.format.DateTimeFormatter;
 
 @Component
 public class DateUtils {
+    /**
+     * Function that convert String to LocalDate
+     *
+     * @param string Date under a String variable
+     * @return LocalDate variable
+     */
     public static LocalDate stringToLocalDate(String string) {
         LocalDate localDate;
         try {
+            // Create the formatter
             DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            // Create the formated date from the String
             localDate = LocalDate.parse(string.replace("/", "-"), dateTimeFormatter);
         } catch (Exception e) {
             localDate = null;
