3045ef21e06c334aa86e40886d5183b24ce1bd6e
Miguel-Gremy <37306845+Miguel-Gremy@users.noreply.github.com>
2021-03-10 15:35:29 +0100

    commit

--- src/main/java/com/cours/clientleger/Controller/Offre/OffreController.java
+++ src/main/java/com/cours/clientleger/Controller/Offre/OffreController.java
@@ -2,7 +2,9 @@ package com.cours.clientleger.Controller.Offre;
 
 import com.cours.clientleger.Application.Offres.Get.OffreGetHandler;
 import com.cours.clientleger.Controller.IndexController;
+import com.cours.clientleger.Model.AccessingDataJPA.OffresInternautesRepository;
 import com.cours.clientleger.Model.AccessingDataJPA.OffresRepository;
+import com.cours.clientleger.Model.DatabaseEntities.OffresInternauteEntity;
 import com.cours.clientleger.Model.Page;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
@@ -26,6 +33,8 @@ public class OffreController {
 
     @Autowired
     OffresRepository offresRepository;
+    @Autowired
+    OffresInternautesRepository offresInternautesRepository;
 
     /**
      * Controller to view the Offres list
@@ -71,6 +80,15 @@ public class OffreController {
      */
     @GetMapping("/postulate/{IdOffre}")
     public ModelAndView OffrePostulate(@PathVariable int IdOffre, HttpSession httpSession) {
+        Map<String, String> data = (Map<String, String>) httpSession.getAttribute("data");
+
+        OffresInternauteEntity offresInternaute = new OffresInternauteEntity();
+        offresInternaute.setIdOffre(IdOffre);
+        offresInternaute.setIdInternaute(Integer.parseInt(data.get("id")));
+        offresInternaute.setDatePostulation(new Date(Calendar.getInstance().getTime().getTime()));
+
+        offresInternautesRepository.save(offresInternaute);
+
         return indexController.Index(httpSession);
     }
 }
